e6cf5b70f844f20af4578be664e32aa7a0d4edcf
Leandro8279 <Leandro8279@gmail.com>
2021-05-05 16:22:48 -0300

    Group by search

--- backend/src/main/java/com/devsuperior/dsvendas/controllers/SaleController.java
+++ backend/src/main/java/com/devsuperior/dsvendas/controllers/SaleController.java
@@ -11,6 +11,8 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.devsuperior.dsvendas.dto.SaleDTO;
+import com.devsuperior.dsvendas.dto.SaleSuccessDTO;
+import com.devsuperior.dsvendas.dto.SaleSumDTO;
 import com.devsuperior.dsvendas.repositories.SaleService;
 
 @RestController
@@ -24,6 +26,18 @@ public class SaleController {
 	public ResponseEntity<Page<SaleDTO>> findAll(Pageable pageable){
 		
 	Page<SaleDTO> list = service.findAll(pageable);
-	return ResponseEntity.ok(list);
+		return ResponseEntity.ok(list);
+	}
+	
+	@GetMapping(value = "/amount-by-seller")
+	public ResponseEntity<List<SaleSumDTO>> amountGroupedBySeller(){	
+		List<SaleSumDTO> list = service.amountGroupedBySeller();
+		return ResponseEntity.ok(list);
+	}
+	
+	@GetMapping(value = "/success-by-seller")
+	public ResponseEntity<List<SaleSuccessDTO>> successGroupedBySeller(){	
+		List<SaleSuccessDTO> list = service.successGroupedBySeller();
+		return ResponseEntity.ok(list);
 	}
 }
--- backend/src/main/java/com/devsuperior/dsvendas/repositories/SellerService.java
+++ backend/src/main/java/com/devsuperior/dsvendas/repositories/SellerService.java
@@ -5,7 +5,9 @@ import java.util.stream.Collectors;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
+import com.devsuperior.dsvendas.dto.SaleSumDTO;
 import com.devsuperior.dsvendas.dto.SellerDTO;
 import com.devsuperior.dsvendas.entities.Seller;
 
@@ -16,12 +18,14 @@ public class SellerService {
 	private SellerRepository repository;
 	
 	
+	@Transactional(readOnly = true)
 	public List<SellerDTO> findAll(){
-		
 		
 		List<Seller> result = repository.findAll();
 		
 		return result.stream().map(x -> new SellerDTO(x)).collect(Collectors.toList());
 	}
+	
+	
 	
 }
